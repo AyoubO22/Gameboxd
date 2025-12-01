1ed41d397d88b6a273df1338e1d06aa3e3d7462f
AyoubO22 <146131883+AyoubO22@users.noreply.github.com>
2025-12-01 15:34:35 +0100

    Initial Commit

---
+++ Gameboxd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gameboxd
+//
+//  Created by Ayoub Ouaadoud on 01/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gameboxd/GameboxdApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameboxdApp.swift
+//  Gameboxd
+//
+//  Created by Ayoub Ouaadoud on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameboxdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
